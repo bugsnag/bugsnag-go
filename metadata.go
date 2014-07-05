7564ec8510badb99698d9c3ad8ad0c3941e7e366
Conrad Irwin <conrad.irwin@gmail.com>
2014-07-05 14:01:05 -0700

    Add meta-data

---
+++ metadata.go
@@ -0,0 +1,151 @@
+package bugsnag
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// MetaData is added to the Bugsnag dashboard in tabs. Each tab is
+// a map of strings -> values. You can add MetaData to an event by
+// passing it whereever rawData is expected (e.g. Notify, AutoNotify)
+type MetaData map[string]map[string]interface{}
+
+// Update the meta-data with more information.
+func (meta MetaData) Update(other MetaData) {
+	for name, tab := range other {
+
+		if meta[name] == nil {
+			meta[name] = make(map[string]interface{})
+		}
+
+		for key, value := range tab {
+			meta[name][key] = value
+		}
+	}
+}
+
+// Add a key-value pair to a tab of Bugsnag meta-data.
+func (meta MetaData) Add(tab string, key string, value interface{}) {
+	if meta[tab] == nil {
+		meta[tab] = make(map[string]interface{})
+	}
+
+	meta[tab][key] = value
+}
+
+// Remove any values from meta-data that have keys matching the filters,
+// and any that are recursive data-structures
+func (meta MetaData) sanitize(filters []string) interface{} {
+	return sanitizer{
+		Filters: filters,
+		Seen:    make([]interface{}, 0),
+	}.Sanitize(meta)
+
+}
+
+// The sanitizer is used to remove filtered params and recursion from meta-data.
+type sanitizer struct {
+	Filters []string
+	Seen    []interface{}
+}
+
+func (s sanitizer) Sanitize(data interface{}) interface{} {
+	for _, s := range s.Seen {
+		// TODO: we don't need deep equal here, just type-ignoring equality
+		if reflect.DeepEqual(data, s) {
+			return "[RECURSION]"
+		}
+	}
+
+	// Sanitizers are passed by value, so we can modify s and it only affects
+	// s.Seen for nested calls.
+	s.Seen = append(s.Seen, data)
+
+	t := reflect.TypeOf(data)
+	v := reflect.ValueOf(data)
+
+	switch t.Kind() {
+	case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64:
+		return data
+
+	case reflect.String:
+		return data
+
+	case reflect.Interface, reflect.Ptr:
+		return s.Sanitize(v.Elem().Interface())
+
+	case reflect.Array, reflect.Slice:
+		ret := make([]interface{}, v.Len())
+		for i := 0; i < v.Len(); i++ {
+			ret[i] = s.Sanitize(v.Index(i).Interface())
+		}
+		return ret
+
+	case reflect.Map:
+		return s.sanitizeMap(v)
+
+	case reflect.Struct:
+		return s.sanitizeStruct(v, t)
+
+		// Things JSON can't serialize:
+	// case t.Chan, t.Func, reflect.Complex64, reflect.Complex128, reflect.UnsafePointer:
+	default:
+		return "[" + t.String() + "]"
+
+	}
+
+}
+
+func (s sanitizer) sanitizeMap(v reflect.Value) interface{} {
+	ret := make(map[string]interface{})
+
+	for _, key := range v.MapKeys() {
+		val := s.Sanitize(v.MapIndex(key).Interface())
+		newKey := fmt.Sprintf("%v", key.Interface())
+
+		if s.shouldRedact(newKey) {
+			val = "[REDACTED]"
+		}
+
+		ret[newKey] = val
+	}
+
+	return ret
+}
+
+// TODO: it might be nice to support JSON tags.
+func (s sanitizer) sanitizeStruct(v reflect.Value, t reflect.Type) interface{} {
+	ret := make(map[string]interface{})
+
+	for i := 0; i < v.NumField(); i++ {
+
+		val := v.Field(i)
+		// Don't export private fields
+		if !val.CanInterface() {
+			continue
+		}
+
+		name := t.Field(i).Name
+
+		if s.shouldRedact(name) {
+			ret[name] = "[REDACTED]"
+		} else {
+			ret[name] = s.Sanitize(val.Interface())
+		}
+	}
+
+	return ret
+}
+
+func (s sanitizer) shouldRedact(key string) bool {
+	for _, filter := range s.Filters {
+		if strings.Contains(strings.ToLower(filter), strings.ToLower(key)) {
+			return true
+		}
+	}
+	return false
+}
---
+++ metadata_test.go
@@ -0,0 +1,156 @@
+package bugsnag
+
+import (
+	"github.com/bugsnag/bugsnag-go/errors"
+	"reflect"
+	"testing"
+	"unsafe"
+)
+
+type _account struct {
+	Id   string
+	Name string
+	Plan struct {
+		Premium bool
+	}
+	secret string
+}
+
+type _broken struct {
+	Me   *_broken
+	Data string
+}
+
+var account = _account{}
+var notifier = NewNotifier(&Configuration{})
+
+func TestMetaDataAdd(t *testing.T) {
+	m := MetaData{
+		"one": {
+			"key":      "value",
+			"override": false,
+		}}
+
+	m.Add("one", "override", true)
+	m.Add("one", "new", "key")
+	m.Add("new", "tab", account)
+
+	if !reflect.DeepEqual(m, MetaData{
+		"one": {
+			"key":      "value",
+			"override": true,
+			"new":      "key",
+		},
+		"new": {
+			"tab": account,
+		},
+	}) {
+		t.Errorf("metadata.Add didn't work: %#v", m)
+	}
+}
+
+func TestMetaDataUpdate(t *testing.T) {
+
+	m := MetaData{
+		"one": {
+			"key":      "value",
+			"override": false,
+		}}
+
+	m.Update(MetaData{
+		"one": {
+			"override": true,
+			"new":      "key",
+		},
+		"new": {
+			"tab": account,
+		},
+	})
+
+	if !reflect.DeepEqual(m, MetaData{
+		"one": {
+			"key":      "value",
+			"override": true,
+			"new":      "key",
+		},
+		"new": {
+			"tab": account,
+		},
+	}) {
+		t.Errorf("metadata.Update didn't work: %#v", m)
+	}
+}
+
+func TestMetaDataSanitize(t *testing.T) {
+
+	var broken = _broken{}
+	broken.Me = &broken
+	broken.Data = "ohai"
+	account.Name = "test"
+	account.Id = "test"
+	account.secret = "hush"
+
+	m := MetaData{
+		"one": {
+			"bool":     true,
+			"int":      7,
+			"float":    7.1,
+			"complex":  complex(1, 1),
+			"func":     func() {},
+			"unsafe":   unsafe.Pointer(broken.Me),
+			"string":   "string",
+			"password": "secret",
+			"array": []hash{{
+				"creditcard": "1234567812345678",
+				"broken":     broken,
+			}},
+			"broken":  broken,
+			"account": account,
+		},
+	}
+
+	n := m.sanitize([]string{"password", "creditcard"})
+
+	if !reflect.DeepEqual(n, map[string]interface{}{
+		"one": map[string]interface{}{
+			"bool":     true,
+			"int":      7,
+			"float":    7.1,
+			"complex":  "[complex128]",
+			"unsafe":   "6",
+			"string":   "string",
+			"func":     "[func()]",
+			"password": "[REDACTED]",
+			"array": []interface{}{map[string]interface{}{
+				"creditcard": "[REDACTED]",
+				"broken": map[string]interface{}{
+					"Me":   "[RECURSION]",
+					"Data": "ohai",
+				},
+			}},
+			"broken": map[string]interface{}{
+				"Me":   "[RECURSION]",
+				"Data": "ohai",
+			},
+			"account": map[string]interface{}{
+				"Id":   "test",
+				"Name": "test",
+				"Plan": map[string]interface{}{
+					"Premium": false,
+				},
+			},
+		},
+	}) {
+		t.Errorf("metadata.Sanitize didn't work: %#v", n)
+	}
+
+}
+
+func ExampleMetaData() {
+	notifier.Notify(errors.Errorf("hi world"),
+		MetaData{"Account": {
+			"id":      account.Id,
+			"name":    account.Name,
+			"paying?": account.Plan.Premium,
+		}})
+}
