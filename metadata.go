beef15af4ec734a3954d3959b069e0bd2b4f6fb0
Kyrylo Silin <kyrylosilin@gmail.com>
2014-07-30 10:48:48 -0700

    metadata: fix formatting
    Not every Emacs mode is perfect :(

--- metadata.go
+++ metadata.go
@@ -87,7 +87,7 @@ func (s sanitizer) Sanitize(data interface{}) interface{} {
 	v := reflect.ValueOf(data)
 
 	switch t.Kind() {
-		case reflect.Bool,
+	case reflect.Bool,
 		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
 		reflect.Float32, reflect.Float64:
