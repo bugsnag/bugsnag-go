e5f738599fef978f0bd882e58db3d52ac551b23f
Facu Ferrer <facundo.j.ferrer@gmail.com>
2015-11-09 16:06:56 +0100

    Add support for stripping subpackages names

--- configuration.go
+++ configuration.go
@@ -135,6 +135,8 @@ func (config *Configuration) stripProjectPackages(file string) string {
 	for _, p := range config.ProjectPackages {
 		if len(p) > 2 && p[len(p)-2] == '/' && p[len(p)-1] == '*' {
 			p = p[:len(p)-1]
+		} else if p[len(p)-1] == '*' && p[len(p)-2] == '*' {
+			p = p[:len(p)-2]
 		} else {
 			p = p + "/"
 		}
