2b6337ec4aee214cc3ce46ee20d20a574ebbb1c7
Graham Campbell <graham@alt-three.com>
2016-08-24 12:17:26 +0100

    Fix typo in Middleware comment  (#37)

--- revel/bugsnagrevel.go
+++ revel/bugsnagrevel.go
@@ -27,7 +27,7 @@ func Filter(c *revel.Controller, fc []revel.Filter) {
 func middleware(event *bugsnag.Event, config *bugsnag.Configuration) error {
 	for _, datum := range event.RawData {
 		if controller, ok := datum.(*revel.Controller); ok {
-			// make the request visible to the builtin HttpMIddleware
+			// make the request visible to the builtin HttpMiddleware
 			event.RawData = append(event.RawData, controller.Request.Request)
 			event.Context = controller.Action
 			event.MetaData.AddStruct("Session", controller.Session)
