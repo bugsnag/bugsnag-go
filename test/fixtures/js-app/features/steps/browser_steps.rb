f7f1207cc5e9d5163251a7a66a1729b617b9f5f6
Delisa Mason <iskanamagus@gmail.com>
2018-01-30 22:36:23 -0800

    chore: Store fixture-specific steps at fixture level
    A couple of tasks required the custom scripts in the browser fixture to
    run, so these belong down in the fixtures.

--- test/fixtures/js-app/features/steps/browser_steps.rb
+++ test/fixtures/js-app/features/steps/browser_steps.rb
@@ -4,3 +4,11 @@ When("I start a local server") do
     And I wait for 6 seconds
   }
 end
+When("I navigate to the {string} page in {string}") do |name, browser|
+  step("I open the URL \"#{create_url_for_fixture(name)}\" in \"#{browser}\"")
+  step("I wait for 2 seconds")
+end
+When("I navigate to the {string} page") do |name|
+  step("I open the URL \"#{create_url_for_fixture(name)}\" in a browser")
+  step("I wait for 2 seconds")
+end
