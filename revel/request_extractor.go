8fd2d413a81c6e27e2861ba13af7df5b4e2e2f8c
Roger Guldbrandsen <roger.guldbrandsen@bugsnag.com>
2018-10-16 14:50:42 -0700

    [chore] Refactor revel header extraction

--- revel/request_extractor.go
+++ revel/request_extractor.go
@@ -11,6 +11,48 @@ type headerExposer interface {
 	GetAll(string) []string
 }
 
+var headers = []string{
+	// Standard headers
+	"Accept",
+	"Accept-Charset",
+	"Accept-Encoding",
+	"Accept-Language",
+	"Accept-Datetime",
+	"Access-Control-Request-Method",
+	"Access-Control-Request-Headers",
+	"Authorization",
+	"Cache-Control",
+	"Connection",
+	"Content-Length",
+	"Content-Type",
+	"Cookie",
+	"Date",
+	"Expect",
+	"Forwarded",
+	"From",
+	"Host",
+	"If-Match",
+	"If-Modified-Since",
+	"If-None-Match",
+	"If-Range",
+	"If-Unmodified-Since",
+	"Max-Forwards",
+	"Origin",
+	"Pragma",
+	"Proxy-Authorization",
+	"Range",
+	"Referer",
+	"TE",
+	"User-Agent",
+	"Upgrade",
+	"Via",
+	"Warning",
+	// Non-standard but common
+	"DNT",
+	"X-Requested-With",
+	"X-CSRF-Token",
+}
+
 func extractRequestData(req *revel.Request, paramsFilters []string) *bugsnag.RequestJSON {
 	return &bugsnag.RequestJSON{
 		ClientIP: req.RemoteAddr,
@@ -26,59 +68,18 @@ func extractRequestData(req *revel.Request, paramsFilters []string) *bugsnag.Req
 
 // extractHeader is a best-guess workaround for pulling out as many headers as possible in a Revel request.
 // Extracts all the standard HTTP headers, and some non-standard, but common headers.
-func extractHeaders(h headerExposer, paramsFilters []string) map[string]string {
+func extractHeaders(exposer headerExposer, paramsFilters []string) map[string]string {
 	m := make(map[string]string)
-	// Standard headers
-	addHeader(paramsFilters, m, h, "Accept")
-	addHeader(paramsFilters, m, h, "Accept-Charset")
-	addHeader(paramsFilters, m, h, "Accept-Encoding")
-	addHeader(paramsFilters, m, h, "Accept-Language")
-	addHeader(paramsFilters, m, h, "Accept-Datetime")
-	addHeader(paramsFilters, m, h, "Access-Control-Request-Method")
-	addHeader(paramsFilters, m, h, "Access-Control-Request-Headers")
-	addHeader(paramsFilters, m, h, "Authorization")
-	addHeader(paramsFilters, m, h, "Cache-Control")
-	addHeader(paramsFilters, m, h, "Connection")
-	addHeader(paramsFilters, m, h, "Content-Length")
-	addHeader(paramsFilters, m, h, "Content-Type")
-	addHeader(paramsFilters, m, h, "Cookie")
-	addHeader(paramsFilters, m, h, "Date")
-	addHeader(paramsFilters, m, h, "Expect")
-	addHeader(paramsFilters, m, h, "Forwarded")
-	addHeader(paramsFilters, m, h, "From")
-	addHeader(paramsFilters, m, h, "Host")
-	addHeader(paramsFilters, m, h, "If-Match")
-	addHeader(paramsFilters, m, h, "If-Modified-Since")
-	addHeader(paramsFilters, m, h, "If-None-Match")
-	addHeader(paramsFilters, m, h, "If-Range")
-	addHeader(paramsFilters, m, h, "If-Unmodified-Since")
-	addHeader(paramsFilters, m, h, "Max-Forwards")
-	addHeader(paramsFilters, m, h, "Origin")
-	addHeader(paramsFilters, m, h, "Pragma")
-	addHeader(paramsFilters, m, h, "Proxy-Authorization")
-	addHeader(paramsFilters, m, h, "Range")
-	addHeader(paramsFilters, m, h, "Referer")
-	addHeader(paramsFilters, m, h, "TE")
-	addHeader(paramsFilters, m, h, "User-Agent")
-	addHeader(paramsFilters, m, h, "Upgrade")
-	addHeader(paramsFilters, m, h, "Via")
-	addHeader(paramsFilters, m, h, "Warning")
-
-	// Non-standard but common
-	addHeader(paramsFilters, m, h, "DNT")
-	addHeader(paramsFilters, m, h, "X-Requested-With")
-	addHeader(paramsFilters, m, h, "X-CSRF-Token")
-	return m
-}
-
-func addHeader(paramsFilters []string, m map[string]string, h headerExposer, headerName string) {
-	if val := h.GetAll(headerName); val != nil {
-		if contains(paramsFilters, strings.ToLower(headerName)) {
-			m[headerName] = "[FILTERED]"
-		} else {
-			m[headerName] = strings.Join(val, ",")
+	for _, header := range headers {
+		if val := exposer.GetAll(header); val != nil {
+			if contains(paramsFilters, strings.ToLower(header)) {
+				m[header] = "[FILTERED]"
+			} else {
+				m[header] = strings.Join(val, ",")
+			}
 		}
 	}
+	return m
 }
 
 func contains(slice []string, sub string) bool {
