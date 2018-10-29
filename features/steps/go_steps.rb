86ec4dcb63511548f043321e47a7e91b03142bb9
Roger Guldbrandsen <roger.guldbrandsen@bugsnag.com>
2018-10-29 14:00:11 +0000

    [maze] Lint fixes

--- features/steps/go_steps.rb
+++ features/steps/go_steps.rb
@@ -1,25 +1,32 @@
 When('I configure the bugsnag endpoint') do
-  steps %Q{
+  steps %(
     When I set environment variable "NOTIFY_ENDPOINT" to "http://localhost:#{MOCK_API_PORT}"
-  }
+  )
 end
 
 When('I run the app {string}') do |app_path|
   run_command(@script_env, "cd features/fixtures/#{app_path}; go run main.go")
 end
 
-Then("the request used payload v4 headers") do
-  steps %Q{
+Then('the request used payload v4 headers') do
+  steps %(
     Then the "bugsnag-api-key" header is not null
     And the "bugsnag-payload-version" header equals "4"
-  }
-  # TODO: format using zulu style RFC3339
+  )
+  # TODO: format using zulu style RFC3339 by adding the following line above:
   # And the "bugsnag-sent-at" header is a timestamp
 end
 
-Then("the request contained the api key {string}") do |api_key|
-  steps %Q{
+Then('the request contained the api key {string}') do |api_key|
+  steps %(
     Then the "bugsnag-api-key" header equals "#{api_key}"
     And the payload field "apiKey" equals "#{api_key}"
-  }
+  )
+end
+
+When('I navigate to the route {string}') do |route|
+  steps %(
+    When I open the URL "http://localhost:#{port}#{route}"
+    And I wait for 1 second
+  )
 end
