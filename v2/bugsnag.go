c659c100e7d5311b8415c45efc4cd2ae8a6f929d
Daria Bialobrzeska <daria.bialobrzeska@smartbear.com>
2024-04-12 13:13:18 +0200

    Update version to 2.4.0

--- v2/bugsnag.go
+++ v2/bugsnag.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version defines the version of this Bugsnag notifier
-const Version = "2.3.1"
+const Version = "2.4.0"
 
 var panicHandlerOnce sync.Once
 var sessionTrackerOnce sync.Once
